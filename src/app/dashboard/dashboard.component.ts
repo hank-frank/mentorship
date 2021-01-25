cb5997a1cb913f345a2434d6754d14a5c1cc381f
Dean Lovett <dean.lovett@gmail.com>
2021-01-25 15:42:13 -0800

    Hocked up the pie charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,12 +13,22 @@ import { PieChartData } from  './pie-chart/pie-chart-data.model'
 export class DashboardComponent implements OnInit {
   @Input() currentUserData: any[any[any]];
   allUserData: any[any];
+  mentorStats : any;
+  menteeStats : any;
+  mentorBarChartData: BarChartData;
+  menteeBarChartData: BarChartData;
+  mentorPieChartData: PieChartData;
+  menteePieChartData: PieChartData;
+  
 
   constructor(private apiService: ApiService) {
     this.currentUserData = apiService.getUserData;
     apiService.getUserData().subscribe((data) => { 
       this.currentUserData = data;    
       this.allUserData = data.allUsers;
+      //console.log(data);
+      this.mentorStats = data.currentUserData.stats.mentor;
+      this.menteeStats = data.currentUserData.stats.mentee;
     }, error => {console.log(`subscription error: `, error)});
   }
 
@@ -29,94 +39,119 @@ export class DashboardComponent implements OnInit {
     this.allUserData.columnHeaders = ["Name", "Is Finished"];
     this.allUserData.tableTitle = "All Users";
     this.allUserData.rowColors = ['#264653', '#E76F51'];
+
+    this.mentorBarChartData = {
+      barValues: [
+        {
+          "name": "Meetings Done",
+          "value": this.mentorStats.meetingsDone
+        },
+        {
+          "name": "Satisfaction",
+          "value": this.mentorStats.satisfaction
+        },
+      ],
+      showXAxisLabel: false,
+      xAxisLabel: '',
+      showYAxisLabel: true,
+      yAxisLabel: 'Percentage',
+      colors: ['#264653', '#2A9D8F'],
+      title: "Mentors"
+    };
+
+    
+    this.menteeBarChartData = {
+      barValues: [
+        {
+          "name": "Meetings Done",
+          "value": this.menteeStats.meetingsDone
+        },
+        {
+          "name": "Satisfaction",
+          "value": this.menteeStats.satisfaction
+        },
+      ],
+      showXAxisLabel: false,
+      xAxisLabel: '',
+      showYAxisLabel: true,
+      yAxisLabel: 'Percentage',
+      colors: ['#F4A261', '#E76F51'],
+      title: "Mentees"
+    };
+
+    this.mentorPieChartData = {
+      pieValues: [
+        {
+          "name": "Not Started",
+          "value": this.mentorStats.notStarted
+        },
+        {
+          "name": "In Progess",
+          "value": this.mentorStats.inProgress
+        },
+        {
+          "name": "Completed",
+          "value": this.mentorStats.completed
+        }
+      ],
+      piegradient: false,
+      pieshowLegend: true ,
+      showLabels: true, 
+      isDoughnut: true,
+      legendPosition: 'below',
+      colors: ['#264653', '#2A9D8F', '#E9C46A'],
+      title: "Mentors"
+    };
+
+    this.menteePieChartData = {
+      pieValues: [
+        {
+          "name": "Not Started",
+          "value": this.menteeStats.notStarted
+        },
+        {
+          "name": "In Progess",
+          "value": this.menteeStats.inProgress
+        },
+        {
+          "name": "Completed",
+          "value": this.menteeStats.completed
+        }
+      ],
+      piegradient: false,
+      pieshowLegend: true ,
+      showLabels: true, 
+      isDoughnut: true,
+      legendPosition: 'below',
+      colors: ['#264653', '#2A9D8F', '#E9C46A'],
+      title: "Mentors"
+    };
+
+    //   pieValues: [
+    //     {
+    //       "name": "Not Started",
+    //       "value": 1 //this.menteeStats.notStated
+    //     },
+    //     {
+    //       "name": "In Progess",
+    //       "value": 2 //this.menteeStats.inProgress
+    //     },
+    //     {
+    //       "name": "Completed",
+    //       "value": 3 //this.menteeStats.completed
+    //     },
+    //   ],
+    //   piegradient: false,
+    //   pieshowLegend: true ,
+    //   showLabels: true, 
+    //   isDoughnut: true,
+    //   legendPosition: 'below',
+    //   colors: ['#E9C46A', '#F4A261', '#E76F51'],
+    //   title: "Mentees"
+    // }; 
     
   }
 
-  mentorBarChartData: BarChartData = {
-    barValues: [
-      {
-        "name": "Meetings Done",
-        "value": 80
-      },
-      {
-        "name": "Satisfaction",
-        "value": 60
-      },
-    ],
-    showXAxisLabel: false,
-    xAxisLabel: '',
-    showYAxisLabel: true,
-    yAxisLabel: 'Percentage',
-    colors: ['#264653', '#2A9D8F'],
-    title: "Mentors"
-  };
-
-  menteeBarChartData: BarChartData = {
-    barValues: [
-      {
-        "name": "Meetings Done",
-        "value": 40
-      },
-      {
-        "name": "Satisfaction",
-        "value": 90
-      },
-    ],
-    showXAxisLabel: false,
-    xAxisLabel: '',
-    showYAxisLabel: true,
-    yAxisLabel: 'Percentage',
-    colors: ['#F4A261', '#E76F51'],
-    title: "Mentees"
-  };
-
-  mentorPieChartData: PieChartData = {
-    pieValues: [
-      {
-        "name": "Not Started",
-        "value": 2
-      },
-      {
-        "name": "In Progess",
-        "value": 3
-      },
-      {
-        "name": "Completed",
-        "value": 4
-      },
-    ],
-    piegradient: false,
-    pieshowLegend: true ,
-    showLabels: true, 
-    isDoughnut: true,
-    legendPosition: 'below',
-    colors: ['#264653', '#2A9D8F', '#E9C46A'],
-    title: "Mentors"
-  }
-
-  menteePieChartData: PieChartData = {
-    pieValues: [
-      {
-        "name": "Not Started",
-        "value": 3
-      },
-      {
-        "name": "In Progess",
-        "value": 9
-      },
-      {
-        "name": "Completed",
-        "value": 4
-      },
-    ],
-    piegradient: false,
-    pieshowLegend: true ,
-    showLabels: true, 
-    isDoughnut: true,
-    legendPosition: 'below',
-    colors: ['#E9C46A', '#F4A261', '#E76F51'],
-    title: "Mentees"
-  }
 
   
 
