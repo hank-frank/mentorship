6fda3d9c0c9b536be2b5f81605cf23157501313f
Shawn Taylor <shawncurtistaylor@gmail.com>
2021-01-28 04:48:27 -0800

    code cleanup

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,45 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-content',
   template: `
 <div class="tableCard">Mentee or Mentor Component</div>
-
-
-
-
-
-
-
-
-
-
 `,
-styleUrls: [ './modal.component.scss']
+  styleUrls: ['./modal.component.scss']
 })
+
 export class ModalContent {
-
-  constructor(public activeModal: NgbActiveModal) {
-    
-   }
+  constructor(public activeModal: NgbActiveModal) { }
 }
-  
-  @Component({
-    selector: 'app-modal',
-    templateUrl: './modal.component.html',
-    styleUrls: ['./modal.component.scss']
-  })
 
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
 
-
-  export class ModalComponent {
-        constructor(private modalService: NgbModal) { }
-    open() {
-      const modalRef = this.modalService.open(ModalContent);
-      modalRef.componentInstance.name = 'World';
+export class ModalComponent {
+  constructor(private modalService: NgbModal) { }
+  open() {
+    const modalRef = this.modalService.open(ModalContent);
+    modalRef.componentInstance.name = 'Shawn';
   }
 }
 
